1d89aa743e9e678adf63bca8002c87bebb2b38e0
Neha Rajaram <rajaramn@spu.edu>
2019-01-07 19:03:57 -0800

    second commit

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,20 @@
-# This is hello world from function.
+# This is hello world from function. This is only function definition. The code will not be executed until the function is called.
 def printFunction():
 	print('hello from function')
+	
+def secondFunction():
+	print('second function printing')
 
+# -------- What is below this line is similar to main() function in c
+# The following code is equivaled to:
+# int main() {
+#	printf("Hello world");
+#  printFunction();
+#  secondFunction();
+# }
+#
 # first line of execution.
 print ('Hello world')
 # second line of execution. This will be called after the first line prints hello workd.
 printFunction()
+secondFunction()
