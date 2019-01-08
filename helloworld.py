4ff7f6a4342a77ab3849e5e9690ca3dcce320f03
Neha Rajaram <rajaramn@spu.edu>
2019-01-07 18:41:33 -0800

    working python hello world

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# This is hello world from function.
+def printFunction():
+	print('hello from function')
+
+# first line of execution.
+print ('Hello world')
+# second line of execution. This will be called after the first line prints hello workd.
+printFunction()
